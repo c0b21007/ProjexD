e39a364a5bce7d8b5490819b6b0552b545641575
Takahiro <c0b21007c4@edu.teu.ac.jp>
2022-10-18 14:06:09 +0900

    練習03-1

---
+++ Ex03/maze.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox as tkm
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    root.geometry("1500x900")
+
+    root.mainloop()
